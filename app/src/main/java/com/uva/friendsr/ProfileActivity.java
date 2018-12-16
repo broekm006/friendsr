a699ca5ad5321dd6d519f6774457f2f98fc5e761
Marc <marc-gw@live.nl>
2018-12-16 15:17:35 +0100

    updated design

--- app/src/main/java/com/uva/friendsr/ProfileActivity.java
+++ app/src/main/java/com/uva/friendsr/ProfileActivity.java
@@ -4,12 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
     Friend retrievedFriend;
